ab2027880fba49f6b67ebf8d1738dd98302bf0b1
Igor Pansini <pansini@outlook.com.br>
2025-08-12 19:20:48 -0300

    created project and config dev properties

---
+++ src/main/java/com/impansini/zip_code/ZipCodeApplication.java
@@ -0,0 +1,13 @@
+package com.impansini.zip_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipCodeApplication.class, args);
+	}
+
+}
